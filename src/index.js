71f928975b284180190fb8baa3d93c19376f7088
Thomas <hollandpirates@gmail.com>
2019-05-01 13:06:29 +0200

    Add life cycle functions

--- src/index.js
+++ src/index.js
@@ -19,13 +19,6 @@ function Welcome(props) {
     return <h1>Hello, {props.name}</h1>;
 }
 
-// Define a component using an ES6 class
-class WelcomeClass extends React.Component {
-    render() {
-        return <h1>Hello, {this.props.name}</h1>;
-    }
-}
-
 // Create element which represents a component (based on Capitals, otherwise it's a html tag)
 const me = <Welcome name="me" />;
 
@@ -35,4 +28,63 @@ function render(element) {
 
 render(me);
 
+// Class components can have state
+
+// Define a component using an ES6 class
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
+render(<Clock />);
+
+
+// Adding lifecycle methods
+
+class UpdatingClock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    // When the clock is rendered to the DOM for the first time: mounting
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
+render(<UpdatingClock />);
 
